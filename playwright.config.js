9cb04ec5002842d29f1cf37cce9ff0cf1f8e85a3
Milka44 <po4tafor4@gmail.com>
2024-08-26 15:34:12 +0300

    changed headless in config

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ const config = defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 use: {
-  headless: true,
+  headless: false,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
   baseURL: process.env.BASE_URL,
